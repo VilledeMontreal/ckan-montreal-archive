f25bedb47f74ce069326f03881c32ddd7e9b1cb8
Paul Walker <starsinmypockets@gmail.com>
2019-08-27 20:51:28 -0400

    [ci][xs]: Run ayrn at frontend-v2

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git clone https://github.com/datopian/frontend-v2.git
 cp -r . frontend-v2/themes/ckan_ng_montreal_theme
 cp frontend-v2/themes/ckan_ng_montreal_theme/.staging.env .env
 cd frontend-v2
-yarn start
+yarn && yarn start
