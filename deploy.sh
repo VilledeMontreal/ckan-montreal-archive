ed127040227549152981edb7a60caf5b9bdd50c2
starsinmypockets <starsinmypockets@gmail.com>
2019-08-30 14:36:08 -0400

    [iframe][m]: Deploy share links

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "hElLo worLd"
 rm -rf frontend-v2
-git clone https://github.com/datopian/frontend-v2.git
+git clone https://github.com/datopian/frontend-v2.git --branch feature/data-explorer-iframe
 cp -r . frontend-v2/themes/ckan_ng_montreal_theme
 cp .staging.env frontend-v2/.env
 cd frontend-v2
